dce27300941fec25f4cc7a0978b61be97b36fc40
Casey Rodarmor <casey@rodarmor.com>
2019-03-27 19:02:00 -0700

    Improve readme and bump version to 0.0.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
-//! `oi` provides a location-annotated error type so you can display useful error messages to your users.
+//! `oi` provides a location-annotated error type so you can display useful
+//! error messages to your users.
 //!
-//! Error messages without location information are often not actionable, especially when they come from a complex program with many potential sources of errors.
+//! Error messages without location information are often not actionable,
+//! especially when they come from a complex program with many potential
+//! sources of errors.
 //!
 //! Compare an unannotated error:
 //!
@@ -16,20 +19,49 @@
 //! Configuration.toml: No such file or directory (os error 2)
 //! ```
 //!
-//! `oi` is named after the exclamation, as in [Oi! Oi! Oi!](https://youtu.be/XWLU76o5rEI). Imagine alerting your users to the location of errors: "Oi! 1.2.3.4 is unreachable!"
+//! `oi` is named after the exclamation, as in [Oi! Oi! Oi!](https://youtu.be/XWLU76o5rEI).
+//! Imagine alerting your users to the location of errors: "Oi! 1.2.3.4 is unreachable!"
+//!
+//! ## usage
+//!
+//! This crate provides an `Error` type that wraps an error and location, a `Location`
+//! trait for error locations, an `ErrAt` trait that extends `Result` with an
+//! `err_at` method to annotate err values with locations, and a `Result<T, E, L>`
+//! type as an alias for the more cumbersome `Result<T, Error<E, L>>`:
+//!
+//! ```rust
+//! pub struct Error<E: Fail, L: Location> {
+//!   pub error: E,
+//!   pub location: L,
+//! }
+//!
+//! pub trait Location: Debug + Send + Sync + 'static {
+//!   fn fmt_error(&self, f: &mut Formatter, error: &dyn Fail) -> fmt::Result;
+//! }
+//!
+//! pub trait ErrAt<T, E: Fail> {
+//!   fn err_at<L: Location, I: Into<L>>(self, location: I) -> Result<T, Error<E, L>>;
+//! }
+//!
+//! pub type Result<T, E, L> = std::result::Result<T, Error<E, L>>;
+//! ```
+//!
+//! `Location` is implemented for `PathBuf` and `SocketAddr`, and can easily be implemented
+//! for custom location types. The one required method, `fmt_error`, gives custom types
+//! control over how an error-annotated location will be rendered to an error message.
 
 mod err_at;
 mod error;
 mod location;
+
+/// Location that an `Error` can occur
+pub use location::Location;
 
 /// Location-annotated error
 pub use error::Error;
 
 /// Extension-trait providing `err_at` convenience method for location-annotating `Result`s
 pub use err_at::ErrAt;
-
-/// Location that an `Error` can occur
-pub use location::Location;
 
 /// Location-annotated Result
 pub type Result<T, E, L> = std::result::Result<T, Error<E, L>>;
